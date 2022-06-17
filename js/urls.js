533aa6a8eb31cbd2b73f5c1c3a591f5c01cf8aac
Shiro Koji <shiro@holygeek.com>
2022-06-17 00:53:11 -0700

    Create urls.js

---
+++ js/urls.js
@@ -0,0 +1,24 @@
+var redirectURLs = {
+	"urls" : [
+	// Articles
+		{ "url" : "https://www.vanityfair.com/news/2022/04/elon-musk-twitter-terrible-things-hes-said-and-done" },
+		{ "url" : "https://www.businessinsider.com/spacex-paid-250000-to-a-flight-attendant-who-accused-elon-musk-of-sexual-misconduct-2022-5" },
+		{ "url" : "https://www.indepthnews.net/index.php/opinion/3735-we-will-coup-whoever-we-want-elon-musk-and-the-overthrow-of-democracy-in-bolivia" },
+		{ "url" : "https://www.independent.co.uk/tech/elon-musk-neuralink-brain-chip-monkey-b2014551.html" },
+		{ "url" : "https://www.protocol.com/tesla-nhtsa-recall" },
+		{ "url" : "https://www.nbcnews.com/tech/tech-news/disgruntled-neighbors-dwindling-shorebirds-jeopardize-spacex-expansion-rcna7792" },
+		{ "url" : "https://www.bloomberg.com/news/articles/2021-12-15/chinese-customers-in-court-over-tesla-legal-strategy-to-silence-critics" },
+		{ "url" : "https://www.theguardian.com/technology/2017/jun/01/tesla-fires-aj-vandermeyden-lawsuit-sexual-harrassment" },
+		{ "url" : "https://www.newsnationnow.com/business/6-more-women-allege-sexual-harassment-at-tesla/" },
+		{ "url" : "https://www.cnbc.com/2021/12/14/former-spacex-engineer-essay-alleges-culture-is-rife-with-sexism.html" },
+		{ "url" : "https://www.inputmag.com/culture/five-ex-spacex-employees-allege-widespread-sexual-misconduct" },
+	// YouTube
+		// Elon Musk’s Neuralink Trials Go HORRIBLY Wrong - TYT
+		{ "url" : "https://www.youtube.com/watch?v=IAsWX8QKROo" },  
+	// Twitter
+		{ "url" : "https://twitter.com/hashtag/elonsucks" },
+		{ "url" : "https://twitter.com/hashtag/elonmusksucks" },
+	// Quora
+		{ "url" : "https://www.quora.com/Whats-the-worst-thing-about-Elon-Musk" }		
+	]
+}
